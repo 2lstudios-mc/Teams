c511b61a8bdfc09dbfe6fc3f0e120ac52fc99553
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-08-15 13:20:16 -0300

    remove classes dependency

--- src/dev/_2lstudios/teams/placeholders/TeamsPlaceholders.java
+++ src/dev/_2lstudios/teams/placeholders/TeamsPlaceholders.java
@@ -1,55 +1,38 @@
 package dev._2lstudios.teams.placeholders;
 
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
-import twolovers.classes.Classes;
-import twolovers.classes.plugin.ClassPlayer;
-
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+
 import dev._2lstudios.teams.enums.Relation;
 import dev._2lstudios.teams.managers.TPlayerManager;
 import dev._2lstudios.teams.managers.TeamManager;
 import dev._2lstudios.teams.managers.TeamsManager;
 import dev._2lstudios.teams.team.TPlayer;
 import dev._2lstudios.teams.team.Team;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 public class TeamsPlaceholders extends PlaceholderExpansion {
   private final Plugin plugin;
-  
   private final TeamsManager teamsManager;
-  
+
   public TeamsPlaceholders(Plugin plugin, TeamsManager teamsManager) {
     this.plugin = plugin;
     this.teamsManager = teamsManager;
   }
-  
-  private boolean isArcherTagged(String name) {
-    Server server = this.plugin.getServer();
-    if (server.getPluginManager().isPluginEnabled("Classes")) {
-      Player player = server.getPlayer(name);
-      if (player != null) {
-        ClassPlayer classPlayer = Classes.getClassPlayerManager().get(player);
-        return classPlayer.hasArcherTag();
-      } 
-    } 
-    return false;
-  }
-  
+
   public String getIdentifier() {
     return "teams";
   }
-  
+
   public String getAuthor() {
     return this.plugin.getDescription().getAuthors().toString();
   }
-  
+
   public String getVersion() {
     return this.plugin.getDescription().getVersion();
   }
-  
+
   public String onPlaceholderRequest(Player player, String identifier) {
     if (player != null && !identifier.isEmpty()) {
       if (identifier.equalsIgnoreCase("team")) {
@@ -60,10 +43,10 @@ public class TeamsPlaceholders extends PlaceholderExpansion {
             String teamName = tPlayer.getTeam();
             Team team = this.teamsManager.getTeamManager().getTeam(teamName);
             if (team != null && team.exists())
-              return team.getDisplayName(); 
+              return team.getDisplayName();
             return "";
-          } 
-        } 
+          }
+        }
       } else if (identifier.startsWith("color_")) {
         TPlayerManager tPlayerManager = this.teamsManager.getTPlayerManager();
         String playerName = player.getName();
@@ -82,19 +65,18 @@ public class TeamsPlaceholders extends PlaceholderExpansion {
                   Team team1 = teamManager.getTeam(targetTeamName);
                   if (team1 != null) {
                     Relation relation = team.getTeamRelations().getMutual(team1);
-                    if (relation == Relation.ENEMY && isArcherTagged(tPlayer1.getName()))
-                      return "&e"; 
+
                     return relation.getPrefix();
-                  } 
-                } 
-              } 
-            } 
-          } 
-        } 
+                  }
+                }
+              }
+            }
+          }
+        }
         return ChatColor.RED.toString();
-      } 
+      }
       return null;
-    } 
+    }
     return null;
   }
 }
